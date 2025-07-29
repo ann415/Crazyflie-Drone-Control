c00249e6290e69eacf81251376546912f2dfccda
Anna Ajeeth <87140127+ann415@users.noreply.github.com>
2025-07-29 14:30:58 +0400

    Update and rename motion_flying.py to main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import logging
+import time
+
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.positioning.motion_commander import MotionCommander
+from cflib.utils import uri_helper
+
+URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
+
+# Only output errors from the logging framework
+logging.basicConfig(level=logging.ERROR)
+
+if __name__ == '__main__':
+    # Initialize the low-level drivers
+    cflib.crtp.init_drivers()
+
+    with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
+        # Set default takeoff height to 15cm (0.15m)
+        with MotionCommander(scf, default_height=0.15) as mc:
+            print("Taking off to 15cm height...")
+            time.sleep(10)  # Wait for stable takeoff
+            print("Landing...")
+            mc.stop()  # Stop movement before landing
